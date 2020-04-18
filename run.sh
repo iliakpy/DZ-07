ebf059cdf2270e83e1f077c3ba3ed493f38a3b94
Bliznetsov Artem <git@iamche.ru>
2020-04-18 22:04:52 +0300

    Замена на alpine

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
 while [[ ! -f /opt/hello-1.0.war ]]; do sleep 5; done
 
-mv /opt/hello-1.0.war /var/lib/tomcat8/webapps/
+mv /opt/hello-1.0.war /usr/local/tomcat/apache-tomcat-8.5.54/webapps
 /usr/share/tomcat8/bin/catalina.sh run
