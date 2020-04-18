c2e1331ef1757d7d9e1f9f925f246b4172acf02c
Bliznetsov Artem <git@iamche.ru>
2020-04-18 22:46:43 +0300

    fix

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 while [[ ! -f /opt/hello-1.0.war ]]; do sleep 5; done
 
 mv /opt/hello-1.0.war /usr/local/tomcat/apache-tomcat-8.5.54/webapps
-/usr/share/tomcat8/bin/catalina.sh run
+/usr/local/tomcat/apache-tomcat-8.5.54/bin/catalina.sh run
